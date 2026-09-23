9a93399c64db87409108b781127863d520546190
Heidi <0266159@up.edu.mx>
2026-09-22 18:03:49 -0600

    first

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float moveSpeed = 8f;
+    private float horizontalInput;
+
+    public float jumpForce = 12f;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    private bool isGrounded;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontalInput * moveSpeed, rb.linearVelocity.y);
+    }
+}
